533ce1db99c5996442c60f0f4dcb00a09c36dc75
nbebeshina <nadia.clairet@gmail.com>
2025-11-04 14:42:29 +0100

    Update text encryption, quantity processing

--- definitions.py
+++ definitions.py
@@ -4,6 +4,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(ROOT_DIR, 'config.yml')
 RESOURCE_DIR = os.path.join(ROOT_DIR, "resources")
 ONTOLOGY_DIR = os.path.join(ROOT_DIR, "ontologies")
+VECTORSTORE_DIR = os.path.join(ROOT_DIR, "chroma")
 INPUT_DIR = os.path.join(ROOT_DIR, 'data/input')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'data/output')
 
